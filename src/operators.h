3f887e1ec75f4bb52c362d67d2623a6b12e2189e
itsgerliz <gerworks.hs@gmail.com>
2024-05-01 23:00:32 +0200

    NEW / Created operators.h (header)

---
+++ src/operators.h
@@ -0,0 +1,20 @@
+#ifndef OPERATORS
+#define OPERATORS
+
+unsigned float addition (unsigned float num1, unsigned float num2) {
+	return (num1 + num2);
+}
+
+unsigned float subtraction (unsigned float num1, unsigned float num2) {
+	return (num1 - num2);
+}
+
+unsigned float multiplication (unsigned float num1, unsigned float num2) {
+	return (num1 * num2);
+}
+
+unsigned float division (unsigned float num1, unsigned float num2) {
+	return (num1 / num2);
+}
+
+#endif
