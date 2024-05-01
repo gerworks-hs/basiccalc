91126f3465222d4816bdd687484889591ace3f1b
itsgerliz <gerworks.hs@gmail.com>
2024-05-01 22:37:02 +0200

    NEW / Finished welcome function

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printWelcome() {
+	printf(
+		"Welcome to the basic calculator\n"
+		"Available operators -> (+ - / *)\n"		
+	);
+}
+
+int main() {
+	printWelcome();	
+	return 0;
+}
